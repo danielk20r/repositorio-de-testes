334bdea42d6847a4ed92f51529878c729f98d321
Daniel Bárbara <daniel.santos.barbara@gmail.com>
2023-08-05 18:16:18 +0100

    update

--- Social/scripts.js
+++ Social/scripts.js
@@ -1,3 +1,22 @@
-function carregar() {
-  var bod = document.getElementById("bodyback");
+  function carregar() {
+  let saudacao = document.getElementById('saudacaoo')
+  let backCores = document.getElementById('headerCores')
+  let horas = new Date()
+  let h = horas.getHours()
+
+   if (h > 6 && h < 13) {
+    saudacao.innerHTML = `Bom dia, seja bem vindo`
+    backCores.style.backgroundImage = 'linear-gradient(to top, black, rgb(118, 210, 238) 50%)'
+   } else if (h >= 13 && h < 20) {
+    saudacao.innerHTML = `Boa Tarde, seja bem vindo`
+    backCores.style.backgroundImage = 'linear-gradient(to top, black, rgb(234, 97, 12) 50%)'
+   } else if (h >= 20 && h < 23) {
+    backCores.style.backgroundImage = 'linear-gradient(to top, black, rgb(47, 0, 98) 50%)'
+     saudacao.innerHTML = `Boa noite, seja bem vindo`
+   } else {
+    backCores.style.backgroundImage = 'linear-gradient(to top, black, rgb(47, 0, 98) 50%)'
+      saudacao.innerHTML = `Boa madrugada, seja bem vindo`
+   }
 }
+
+
---
+++ tentativaCalcSimples/script.js
@@ -0,0 +1,4 @@
+const numeros = document.getElementsByClassName('n1, n2, n3, simboloMais, botaoIgual')
+
+console.log(numeros)
+
