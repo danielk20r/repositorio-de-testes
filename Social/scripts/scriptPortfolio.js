2cfd92048564404c4526504fb9ec310b44fb2ccd
Daniel Bárbara <daniel.santos.barbara@gmail.com>
2023-08-11 23:41:14 +0100

    update

--- Social/scripts/scriptPortfolio.js
+++ Social/scripts/scriptPortfolio.js
@@ -1,23 +1,3 @@
-// var nome = prompt("Escreve o Teu nome para uma melhor experiencia");
-//       alert('Bem vindo ao meu site, ' + nome + '!')
-
-// function mudouTamanho (){
-//     if (window.innerWidth >= 768) {
-//         itens.style.display = 'block'
-//     } else {
-//         itens.style.display = 'none'
-//     }
-// }
-
-// function clickMenu () {
-//     if (itens.style.display == 'block') {
-//         itens.style.display = 'none'
-//         } else {
-//             itens.style.display = 'block'
-//         }
-// }
-
-
 function escreverNome() {
 let nome = prompt('Escreve o teu nome')
 let escreverNome = document.getElementById('nomePessoa')
